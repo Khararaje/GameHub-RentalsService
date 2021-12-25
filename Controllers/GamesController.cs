ac06e48e393866f3607228d88d217cb2a620f971
Kharajae Million <Milliokp@mail.uc.edu>
2021-12-24 22:13:19 -0500

    Add project files.

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameHubRentals.Models;
+
+namespace GameHubRentals.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Random()
+        {
+            var game = new Game() {Id = 1, Name = "Vangaurd" };
+            return View(game);
+        }
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameHubRentals.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Platform { get; set; }
+        public string Name { get; set; }
+        public int rating { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public bool inStock { get; set; }
+        public int quantity { get; set; }
+
+        
+    }
+}
